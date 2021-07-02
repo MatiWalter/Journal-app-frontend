d4b69afa55885eec82f169cc229190d10c38ecf9
Matias Walter <matiwalter97@hotmail.com>
2021-07-02 20:14:16 -0300

    add minor changes

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { JournalApp } from './JournalApp';
+import { JournalApp } from './src/JournalApp';
 import './src/styles/styles.scss';
 
 ReactDOM.render(
